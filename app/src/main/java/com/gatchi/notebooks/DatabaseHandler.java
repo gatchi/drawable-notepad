705f770afb301012849338272ee77593e99c14a9
gatchi <christen.got@gmail.com>
2017-07-22 16:29:57 -0700

    chancged spaces to tabs on DatabaseHandler.java

--- app/src/main/java/com/gatchi/notebooks/DatabaseHandler.java
+++ app/src/main/java/com/gatchi/notebooks/DatabaseHandler.java
@@ -23,273 +23,273 @@ import java.util.Locale;
 public class DatabaseHandler extends SQLiteOpenHelper {
 
 
-    private static final int DATABASE_VERSION = 3;
-    private static final String DATABASE_NAME = "notepadDatabase";
-    private static final String TABLE_NOTES = "notes";
-    private static final String KEY_ID = "id";
-    private static final String KEY_NOTE_TITLE = "noteTitle";
-    private static final String KEY_SPANNABLE_NOTE = "serializedSpannableNote";
-    private static final String KEY_IMAGE = "image";
-    private static final String KEY_DATE_UPDATED = "dateUpdated";
-    private static final DateFormat dt = new SimpleDateFormat("dd.MM.yyyy, hh:mm:ss", Locale.getDefault());
+	private static final int DATABASE_VERSION = 3;
+	private static final String DATABASE_NAME = "notepadDatabase";
+	private static final String TABLE_NOTES = "notes";
+	private static final String KEY_ID = "id";
+	private static final String KEY_NOTE_TITLE = "noteTitle";
+	private static final String KEY_SPANNABLE_NOTE = "serializedSpannableNote";
+	private static final String KEY_IMAGE = "image";
+	private static final String KEY_DATE_UPDATED = "dateUpdated";
+	private static final DateFormat dt = new SimpleDateFormat("dd.MM.yyyy, hh:mm:ss", Locale.getDefault());
 
-    public DatabaseHandler(Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
-    }
+	public DatabaseHandler(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
 
 	/**
 	 * Creates a new database for storing notes if one doesn't exist.
 	 */
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE IF NOT EXISTS " + TABLE_NOTES + "("
-                + KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-                + KEY_SPANNABLE_NOTE + " TEXT, "
-                + KEY_IMAGE + " BLOB, "
-                + KEY_DATE_UPDATED + " TEXT, "
-                + KEY_NOTE_TITLE + " VARCHAR(100))" //We don't want a super long title...
-                );
-    }
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL("CREATE TABLE IF NOT EXISTS " + TABLE_NOTES + "("
+				+ KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+				+ KEY_SPANNABLE_NOTE + " TEXT, "
+				+ KEY_IMAGE + " BLOB, "
+				+ KEY_DATE_UPDATED + " TEXT, "
+				+ KEY_NOTE_TITLE + " VARCHAR(100))" //We don't want a super long title...
+				);
+	}
 
 	/**
 	 * Handles changes to the database schema by automatically updating the old.
 	 */
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
-        switch (oldVersion)
-        {
-            case 1:
-                //upgrade from version 1 to 2
-                db.execSQL("ALTER TABLE " + TABLE_NOTES + " ADD COLUMN " + KEY_DATE_UPDATED + " TEXT;");
-            case 2:
-                //upgrade from version 2 to 3
-                db.execSQL("ALTER TABLE " + TABLE_NOTES + " ADD COLUMN " + KEY_NOTE_TITLE + " VARCHAR(100);");
-            case 3:
-                //upgrade from version 3 to 4
-                //db.execSQL();
+		switch (oldVersion)
+		{
+			case 1:
+				//upgrade from version 1 to 2
+				db.execSQL("ALTER TABLE " + TABLE_NOTES + " ADD COLUMN " + KEY_DATE_UPDATED + " TEXT;");
+			case 2:
+				//upgrade from version 2 to 3
+				db.execSQL("ALTER TABLE " + TABLE_NOTES + " ADD COLUMN " + KEY_NOTE_TITLE + " VARCHAR(100);");
+			case 3:
+				//upgrade from version 3 to 4
+				//db.execSQL();
 
-                //and so on.. do not add breaks so that switch will
-                //start at oldVersion, and run straight through to the latest
+				//and so on.. do not add breaks so that switch will
+				//start at oldVersion, and run straight through to the latest
 
-        }
-    }
-
-    /**
-     * Method used to clear notes table
-     */
-    public void clearAllNotes() {
-        SQLiteDatabase db = getWritableDatabase();
-        db.execSQL("DELETE FROM " + TABLE_NOTES);
-    }
-
-    /**
-     * Method used to put Note object into Database
-     * @param note Note object to put into DB
-     */
-    public void createNote(Note note) {
-        SQLiteDatabase db = getWritableDatabase();
-        String spannableAsHtml = Html.toHtml(note.getSpannable());
-        String date = dt.format(new Date());
-
-        ContentValues values = new ContentValues();
-
-        values.put(KEY_SPANNABLE_NOTE, spannableAsHtml);
-        values.put(KEY_NOTE_TITLE, note.getTitle());
-        values.put(KEY_IMAGE, BitmapConverter.getBytes(note.getImage()));
-        values.put(KEY_DATE_UPDATED, date);
-        db.insert(TABLE_NOTES, null, values);
-        db.close();
-    }
-
-    /**
-     * Method used to get specified Note from Database
-     * @param id KEY_ID of Note to get from Database
-     * @return Note object with specified KEY_ID
-     */
-    public Note getNote(int id) throws SQLiteException {
-        SQLiteDatabase db = getReadableDatabase();
-
-        Cursor cursor = db.query(TABLE_NOTES, new String[]{KEY_ID, KEY_SPANNABLE_NOTE, KEY_IMAGE, KEY_DATE_UPDATED, KEY_NOTE_TITLE}, KEY_ID + "=?",
-                new String[]{String.valueOf(id)}, null, null, null, null);
-
-        if (!cursor.moveToFirst()) {
-            deleteNote(id); //Failed to load note. Maybe the user restored data from an incompatible backup?
-            //Let's delete the problematic data
-            throw new SQLiteException("Note doesn't exist");
-        }
-
-        String spannableAsHtml = cursor.getString(cursor.getColumnIndex(KEY_SPANNABLE_NOTE));
-        // :)))))
-        Spannable spannable = (Spannable) Html.fromHtml(Html.toHtml(Html.fromHtml(spannableAsHtml)));
-
-        Bitmap image = BitmapConverter.getImage(cursor.getBlob(cursor.getColumnIndex(KEY_IMAGE)));
-
-        //Default val
-        Date date;
-
-        try {
-            date = dt.parse(cursor.getString(cursor.getColumnIndex(KEY_DATE_UPDATED)));
-        } catch (Exception e) {
-            date = new Date();
-            e.printStackTrace();
-        }
-
-        String title;
-        try {
-            title = cursor.getString(cursor.getColumnIndex(KEY_NOTE_TITLE));
-        }catch (Exception e){
-            title = "";
-            e.printStackTrace();
-        }
-
-        if (spannable.length() >= 2) {
-            spannable = (Spannable) spannable.subSequence(0, spannable.length() - 2);
-        }
-
-
-        db.close();
-        cursor.close();
-        return new Note(id, title, spannable, image, date);
-    }
-
-    /**
-     * Method used to delete specified Note from Database
-     * @param note Note to delete
-     */
-    public void deleteNote(Note note) {
-        deleteNote(note.getId());
-    }
+		}
+	}
 
 	/**
-     * Method used to delete specified Note from Database
-     * @param note Note to delete
-     */
-    public void deleteNote(int noteId) {
-        SQLiteDatabase db = getWritableDatabase();
-        db.delete(TABLE_NOTES, KEY_ID + "=?", new String[]{String.valueOf(noteId)});
-        db.close();
-    }
+	 * Method used to clear notes table
+	 */
+	public void clearAllNotes() {
+		SQLiteDatabase db = getWritableDatabase();
+		db.execSQL("DELETE FROM " + TABLE_NOTES);
+	}
 
-    /**
-     * Method used to get count of notes in Database
-     * @return count of notes in Database
-     */
-    public int getNoteCount() {
-        SQLiteDatabase db = getReadableDatabase();
-        int numberOfNotes = (int) DatabaseUtils.queryNumEntries(db, TABLE_NOTES);
-        db.close();
-        return numberOfNotes;
-    }
+	/**
+	 * Method used to put Note object into Database
+	 * @param note Note object to put into DB
+	 */
+	public void createNote(Note note) {
+		SQLiteDatabase db = getWritableDatabase();
+		String spannableAsHtml = Html.toHtml(note.getSpannable());
+		String date = dt.format(new Date());
 
-    /**
-     * Method used to update Note's text/format
-     * @param note Note to update
-     * @return updated Note
-     */
-    public int updateNote(Note note) {
-        SQLiteDatabase db = getWritableDatabase();
+		ContentValues values = new ContentValues();
 
-        String spannableAsHtml = Html.toHtml(note.getSpannable());
+		values.put(KEY_SPANNABLE_NOTE, spannableAsHtml);
+		values.put(KEY_NOTE_TITLE, note.getTitle());
+		values.put(KEY_IMAGE, BitmapConverter.getBytes(note.getImage()));
+		values.put(KEY_DATE_UPDATED, date);
+		db.insert(TABLE_NOTES, null, values);
+		db.close();
+	}
 
-        String date = dt.format(new Date());
+	/**
+	 * Method used to get specified Note from Database
+	 * @param id KEY_ID of Note to get from Database
+	 * @return Note object with specified KEY_ID
+	 */
+	public Note getNote(int id) throws SQLiteException {
+		SQLiteDatabase db = getReadableDatabase();
 
-        ContentValues values = new ContentValues();
-        values.put(KEY_IMAGE, BitmapConverter.getBytes(note.getImage()));
-        values.put(KEY_DATE_UPDATED, date);
-        values.put(KEY_SPANNABLE_NOTE, spannableAsHtml);
-        values.put(KEY_NOTE_TITLE, note.getTitle());
+		Cursor cursor = db.query(TABLE_NOTES, new String[]{KEY_ID, KEY_SPANNABLE_NOTE, KEY_IMAGE, KEY_DATE_UPDATED, KEY_NOTE_TITLE}, KEY_ID + "=?",
+				new String[]{String.valueOf(id)}, null, null, null, null);
 
-        return db.update(TABLE_NOTES, values, KEY_ID + "=?", new String[]{String.valueOf(note.getId())});
-    }
+		if (!cursor.moveToFirst()) {
+			deleteNote(id); //Failed to load note. Maybe the user restored data from an incompatible backup?
+			//Let's delete the problematic data
+			throw new SQLiteException("Note doesn't exist");
+		}
 
-    /**
-     * Method used to get all notes in Database
-     * @return ArrayList of Notes, containing all notes in Database
-     */
-    public ArrayList<Note> getAllNotesAsArrayList() {
-        ArrayList<Note> notes = new ArrayList<>();
+		String spannableAsHtml = cursor.getString(cursor.getColumnIndex(KEY_SPANNABLE_NOTE));
+		// :)))))
+		Spannable spannable = (Spannable) Html.fromHtml(Html.toHtml(Html.fromHtml(spannableAsHtml)));
 
-        SQLiteDatabase db = getWritableDatabase();
-        Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_NOTES, null);
+		Bitmap image = BitmapConverter.getImage(cursor.getBlob(cursor.getColumnIndex(KEY_IMAGE)));
 
-        if (cursor.moveToFirst()) {
-            do {
-                int id = Integer.parseInt(cursor.getString(cursor.getColumnIndex(KEY_ID)));
-                Spannable spannable = (Spannable) Html.fromHtml(cursor.getString(cursor.getColumnIndex(KEY_SPANNABLE_NOTE)));
-                Bitmap image = BitmapConverter.getImage(cursor.getBlob(cursor.getColumnIndex(KEY_IMAGE)));
-                //Default val
-                Date date;
+		//Default val
+		Date date;
 
-                try {
-                    date = dt.parse(cursor.getString(cursor.getColumnIndex(KEY_DATE_UPDATED)));
-                } catch (Exception e) {
-                    date = new Date();
-                    e.printStackTrace();
-                }
+		try {
+			date = dt.parse(cursor.getString(cursor.getColumnIndex(KEY_DATE_UPDATED)));
+		} catch (Exception e) {
+			date = new Date();
+			e.printStackTrace();
+		}
 
-                String title;
-                try {
-                    title = cursor.getString(cursor.getColumnIndex(KEY_NOTE_TITLE));
-                } catch (Exception e) {
-                    title = "";
-                    e.printStackTrace();
-                }
+		String title;
+		try {
+			title = cursor.getString(cursor.getColumnIndex(KEY_NOTE_TITLE));
+		}catch (Exception e){
+			title = "";
+			e.printStackTrace();
+		}
 
-                Note note = new Note(id, title, spannable, image, date);
-                notes.add(note);
-            }
-            while (cursor.moveToNext());
-        }
-        cursor.close();
-        return notes;
-    }
+		if (spannable.length() >= 2) {
+			spannable = (Spannable) spannable.subSequence(0, spannable.length() - 2);
+		}
 
-    /**
-     * Method used to get all notes in Database
-     * @return Array of Notes, containing all notes in Database
-     */
-    public Note[] getAllNotesAsArray() {
-        ArrayList<Note> notes = new ArrayList<>();
 
-        SQLiteDatabase db = getWritableDatabase();
-        Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_NOTES, null);
+		db.close();
+		cursor.close();
+		return new Note(id, title, spannable, image, date);
+	}
 
-        if (cursor.moveToFirst()) {
-            do {
-                int id = Integer.parseInt(cursor.getString(cursor.getColumnIndex(KEY_ID)));
-                Spannable spannable = (Spannable) Html.fromHtml(cursor.getString(cursor.getColumnIndex(KEY_SPANNABLE_NOTE)));
-                Bitmap image = BitmapConverter.getImage(cursor.getBlob(cursor.getColumnIndex(KEY_IMAGE)));
-                //Default val
-                Date date;
+	/**
+	 * Method used to delete specified Note from Database
+	 * @param note Note to delete
+	 */
+	public void deleteNote(Note note) {
+		deleteNote(note.getId());
+	}
 
-                try {
-                    date = dt.parse(cursor.getString(cursor.getColumnIndex(KEY_DATE_UPDATED)));
-                } catch (Exception e) {
-                    date = new Date();
-                    e.printStackTrace();
-                }
+	/**
+	 * Method used to delete specified Note from Database
+	 * @param note Note to delete
+	 */
+	public void deleteNote(int noteId) {
+		SQLiteDatabase db = getWritableDatabase();
+		db.delete(TABLE_NOTES, KEY_ID + "=?", new String[]{String.valueOf(noteId)});
+		db.close();
+	}
 
-                String title;
-                try {
-                    title = cursor.getString(cursor.getColumnIndex(KEY_NOTE_TITLE));
-                } catch (Exception e) {
-                    title = "";
-                    e.printStackTrace();
-                }
+	/**
+	 * Method used to get count of notes in Database
+	 * @return count of notes in Database
+	 */
+	public int getNoteCount() {
+		SQLiteDatabase db = getReadableDatabase();
+		int numberOfNotes = (int) DatabaseUtils.queryNumEntries(db, TABLE_NOTES);
+		db.close();
+		return numberOfNotes;
+	}
 
-                Note note = new Note(id, title, spannable, image, date);
-                notes.add(note);
-            }
-            while (cursor.moveToNext());
-        }
+	/**
+	 * Method used to update Note's text/format
+	 * @param note Note to update
+	 * @return updated Note
+	 */
+	public int updateNote(Note note) {
+		SQLiteDatabase db = getWritableDatabase();
 
-        Note[] result = new Note[notes.size()];
+		String spannableAsHtml = Html.toHtml(note.getSpannable());
 
-        for (int i = 0; i < notes.size(); i++) {
-            result[i] = notes.get(i);
-        }
-        cursor.close();
-        return result;
-    }
+		String date = dt.format(new Date());
+
+		ContentValues values = new ContentValues();
+		values.put(KEY_IMAGE, BitmapConverter.getBytes(note.getImage()));
+		values.put(KEY_DATE_UPDATED, date);
+		values.put(KEY_SPANNABLE_NOTE, spannableAsHtml);
+		values.put(KEY_NOTE_TITLE, note.getTitle());
+
+		return db.update(TABLE_NOTES, values, KEY_ID + "=?", new String[]{String.valueOf(note.getId())});
+	}
+
+	/**
+	 * Method used to get all notes in Database
+	 * @return ArrayList of Notes, containing all notes in Database
+	 */
+	public ArrayList<Note> getAllNotesAsArrayList() {
+		ArrayList<Note> notes = new ArrayList<>();
+
+		SQLiteDatabase db = getWritableDatabase();
+		Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_NOTES, null);
+
+		if (cursor.moveToFirst()) {
+			do {
+				int id = Integer.parseInt(cursor.getString(cursor.getColumnIndex(KEY_ID)));
+				Spannable spannable = (Spannable) Html.fromHtml(cursor.getString(cursor.getColumnIndex(KEY_SPANNABLE_NOTE)));
+				Bitmap image = BitmapConverter.getImage(cursor.getBlob(cursor.getColumnIndex(KEY_IMAGE)));
+				//Default val
+				Date date;
+
+				try {
+					date = dt.parse(cursor.getString(cursor.getColumnIndex(KEY_DATE_UPDATED)));
+				} catch (Exception e) {
+					date = new Date();
+					e.printStackTrace();
+				}
+
+				String title;
+				try {
+					title = cursor.getString(cursor.getColumnIndex(KEY_NOTE_TITLE));
+				} catch (Exception e) {
+					title = "";
+					e.printStackTrace();
+				}
+
+				Note note = new Note(id, title, spannable, image, date);
+				notes.add(note);
+			}
+			while (cursor.moveToNext());
+		}
+		cursor.close();
+		return notes;
+	}
+
+	/**
+	 * Method used to get all notes in Database
+	 * @return Array of Notes, containing all notes in Database
+	 */
+	public Note[] getAllNotesAsArray() {
+		ArrayList<Note> notes = new ArrayList<>();
+
+		SQLiteDatabase db = getWritableDatabase();
+		Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_NOTES, null);
+
+		if (cursor.moveToFirst()) {
+			do {
+				int id = Integer.parseInt(cursor.getString(cursor.getColumnIndex(KEY_ID)));
+				Spannable spannable = (Spannable) Html.fromHtml(cursor.getString(cursor.getColumnIndex(KEY_SPANNABLE_NOTE)));
+				Bitmap image = BitmapConverter.getImage(cursor.getBlob(cursor.getColumnIndex(KEY_IMAGE)));
+				//Default val
+				Date date;
+
+				try {
+					date = dt.parse(cursor.getString(cursor.getColumnIndex(KEY_DATE_UPDATED)));
+				} catch (Exception e) {
+					date = new Date();
+					e.printStackTrace();
+				}
+
+				String title;
+				try {
+					title = cursor.getString(cursor.getColumnIndex(KEY_NOTE_TITLE));
+				} catch (Exception e) {
+					title = "";
+					e.printStackTrace();
+				}
+
+				Note note = new Note(id, title, spannable, image, date);
+				notes.add(note);
+			}
+			while (cursor.moveToNext());
+		}
+
+		Note[] result = new Note[notes.size()];
+
+		for (int i = 0; i < notes.size(); i++) {
+			result[i] = notes.get(i);
+		}
+		cursor.close();
+		return result;
+	}
 }
