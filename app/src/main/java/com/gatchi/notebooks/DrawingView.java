4ff8fea5dd96399ddbfd4231f716c7786013928a
gatchi <christen.got@gmail.com>
2017-07-22 16:40:24 -0700

    spaces -> tabs, increased readibility and consistency

--- app/src/main/java/com/gatchi/notebooks/DrawingView.java
+++ app/src/main/java/com/gatchi/notebooks/DrawingView.java
@@ -18,25 +18,25 @@ import android.view.View;
  */
 public class DrawingView extends View {
 
-    /// initial color
-    private static final int DEFAULT_PAINT_COLOR = Color.BLACK;
-    /// drawing path
-    private Path drawPath;
-    /// drawing and canvas paint
-    private Paint drawPaint, canvasPaint;
-    /// canvas
-    private Canvas drawCanvas;
-    /// canvas bitmap
-    private Bitmap canvasBitmap;
-    /// brush size
-    private float brushSize;
-    /// erase mode
-    private boolean erase;
+	/// initial color
+	private static final int DEFAULT_PAINT_COLOR = Color.BLACK;
+	/// drawing path
+	private Path drawPath;
+	/// drawing and canvas paint
+	private Paint drawPaint, canvasPaint;
+	/// canvas
+	private Canvas drawCanvas;
+	/// canvas bitmap
+	private Bitmap canvasBitmap;
+	/// brush size
+	private float brushSize;
+	/// erase mode
+	private boolean erase;
 
-    public DrawingView(Context context, AttributeSet attrs){
-        super(context, attrs);
-        setupDrawing();
-    }
+	public DrawingView(Context context, AttributeSet attrs){
+		super(context, attrs);
+		setupDrawing();
+	}
 
 	/**
 	 * Settings.
@@ -46,24 +46,22 @@ public class DrawingView extends View {
 	 * generate the drawing for the first time and in subsequent
 	 * edits.
 	 */
-    private void setupDrawing(){
-        drawPath = new Path();
-        drawPaint = new Paint();
+	private void setupDrawing(){
+		drawPath = new Path();
+		drawPaint = new Paint();
 
-        setBrushSize(10);
+		setBrushSize(10);
 
-        drawPaint.setColor(DEFAULT_PAINT_COLOR);
+		drawPaint.setColor(DEFAULT_PAINT_COLOR);
 
-        drawPaint.setAntiAlias(true);
-        drawPaint.setStrokeWidth(brushSize);
-        drawPaint.setStyle(Paint.Style.STROKE);
-        drawPaint.setStrokeJoin(Paint.Join.ROUND);
-        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+		drawPaint.setAntiAlias(true);
+		drawPaint.setStrokeWidth(brushSize);
+		drawPaint.setStyle(Paint.Style.STROKE);
+		drawPaint.setStrokeJoin(Paint.Join.ROUND);
+		drawPaint.setStrokeCap(Paint.Cap.ROUND);
 
-        canvasPaint = new Paint(Paint.DITHER_FLAG);
-
-
-    }
+		canvasPaint = new Paint(Paint.DITHER_FLAG);
+	}
 
 	/**
 	 * Why is this method like this ??
@@ -73,19 +71,19 @@ public class DrawingView extends View {
 	 * if there is 
 	 * @todo Figure out why this is like this.
 	 */
-    @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        super.onSizeChanged(w, h, oldw, oldh);
-        if (canvasBitmap == null) {
-            canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
-            drawCanvas = new Canvas(canvasBitmap);
-        }
-        else {
-            canvasBitmap = Bitmap.createBitmap(canvasBitmap);
-            canvasBitmap = canvasBitmap.copy(Bitmap.Config.ARGB_8888, true);
-            drawCanvas = new Canvas(canvasBitmap); // repeated, move line down
-        }
-    }
+	@Override
+	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+		super.onSizeChanged(w, h, oldw, oldh);
+		if (canvasBitmap == null) {
+			canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+			drawCanvas = new Canvas(canvasBitmap);
+		}
+		else {
+			canvasBitmap = Bitmap.createBitmap(canvasBitmap);
+			canvasBitmap = canvasBitmap.copy(Bitmap.Config.ARGB_8888, true);
+			drawCanvas = new Canvas(canvasBitmap); // repeated, move line down
+		}
+	}
 
 	/**
 	 * See details.
@@ -97,99 +95,95 @@ public class DrawingView extends View {
 	 * what prep is needed.
 	 * @todo Give this a better description.
 	 */
-    @Override
-    protected void onDraw(Canvas canvas) {
+	@Override
+	protected void onDraw(Canvas canvas) {
 		// Numbers are position offsets from left and top
-        canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
-        canvas.drawPath(drawPath, drawPaint);
-    }
+		canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
+		canvas.drawPath(drawPath, drawPaint);
+	}
 
 	/**
 	 * Translates touches to draw strokes.
 	 * @todo Make paint appear directly under finger.
 	 */
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
 		// TODO: Add offsets to X and Y to make paint position more accurate
-        float touchX = event.getX();
-        float touchY = event.getY();
+		float touchX = event.getX();
+		float touchY = event.getY();
 
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                drawPath.moveTo(touchX, touchY);
-                break;
-            case MotionEvent.ACTION_MOVE:
-                drawPath.lineTo(touchX, touchY);
-                break;
-            case MotionEvent.ACTION_UP:
-                drawCanvas.drawPath(drawPath, drawPaint);
-                drawPath.reset();
-                break;
-            default:
-                return false;
-
-        }
-        invalidate();
-        return true;
-    }
+		switch (event.getAction()) {
+			case MotionEvent.ACTION_DOWN:
+				drawPath.moveTo(touchX, touchY);
+				break;
+			case MotionEvent.ACTION_MOVE:
+				drawPath.lineTo(touchX, touchY);
+				break;
+			case MotionEvent.ACTION_UP:
+				drawCanvas.drawPath(drawPath, drawPaint);
+				drawPath.reset();
+				break;
+			default:
+				return false;
+		}
+		invalidate();
+		return true;
+	}
 
 	/**
 	 * Clears the drawing from the view.
 	 * Used by NoteActivity::wipeCanvas().
 	 */
-    public void startNew(){
-        drawCanvas.drawColor(0, PorterDuff.Mode.CLEAR);
-        invalidate();  // forces the view to redraw
-    }
+	public void startNew(){
+		drawCanvas.drawColor(0, PorterDuff.Mode.CLEAR);
+		invalidate();  // forces the view to redraw
+	}
 
 	/**
 	 * Set paint color.
 	 * Set paint color using Android Color system.
 	 * @todo Explain in docs how Android Color system works.
 	 */
-    public void setPaintColor(int paintColor) {
-        drawPaint.setColor(paintColor);
-    }
+	public void setPaintColor(int paintColor) {
+		drawPaint.setColor(paintColor);
+	}
 
 	/**
 	 * Set brush size.
 	 * @todo Docs: uses a float - but what do those values map to?
 	 */
-    public void setBrushSize(float size) {
-        brushSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
-                size, getResources().getDisplayMetrics());
-        drawPaint.setStrokeWidth(brushSize);
-
-    }
+	public void setBrushSize(float size) {
+		brushSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, size, getResources().getDisplayMetrics());
+		drawPaint.setStrokeWidth(brushSize);
+	}
 
 	/**
 	 * Changes paintbrush to eraser.
 	 * @todo Make erase immediate (currently doesnt erase until finger is lifted)
 	 */
-    public void setErase(boolean isErase){
-        erase = isErase;
-
-        if(erase) {
-            drawPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
-        }
-        else {
-            drawPaint.setXfermode(null);
-        }
-    }
+	public void setErase(boolean isErase){
+		erase = isErase;
+		if(erase) {
+			drawPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
+		}
+		else {
+			drawPaint.setXfermode(null);
+		}
+	}
 
 	/**
 	 * Retrieves the canvas bitmap.
 	 * Used by NoteActivity::saveOrUpdateNote.
 	 */
-    public Bitmap getCanvasBitmap() {
-        return canvasBitmap;
-    }
+	public Bitmap getCanvasBitmap() {
+		return canvasBitmap;
+	}
 
 	/**
 	 * Replaces canvas bitmap with already created one.
 	 * Used by NoteActivity::loadNote.
 	 */
-    public void setBitmap(Bitmap bmp) {
-        canvasBitmap = bmp;
-    }
+	public void setBitmap(Bitmap bmp) {
+		canvasBitmap = bmp;
+	}
 }
