235d981b40e31531123a7b26a78ddd63ffaa47d2
gatchi <christen.got@gmail.com>
2017-07-21 21:25:14 -0700

    finished documenting Note.java

--- app/src/main/java/com/gatchi/notebooks/Note.java
+++ app/src/main/java/com/gatchi/notebooks/Note.java
@@ -19,19 +19,19 @@ public class Note {
     // Spannable used to format text
     private final Spannable mSpannable;
 
-    //
+    // Title of note
     private final String mTitle;
 
     // Raw text used to make titles
     private final String rawText;
 
-    //Painting
+    // Painting
     private final Bitmap mImage;
 
-    //Dates (Default value)
+    // Dates (Default value)
     private Date dateUpdated = new Date();
 
-    //Formatter
+    // Formatter
     private static final DateFormat dt = new SimpleDateFormat("dd.MM.yyyy, hh:mm:ss", Locale.getDefault());
 
 
@@ -47,30 +47,53 @@ public class Note {
         this.dateUpdated = dateUpdated;
     }
 
+	/**
+	 * Returns the unique ID number of the note.
+	 */
     public int getId() {
         return mId;
     }
 
+	/**
+	 * Returns the note title.
+	 */
     public String getTitle(){
         return mTitle;
     }
 
+	/**
+	 * Returns the formatted body of note text.
+	 */
     public Spannable getSpannable() {
         return mSpannable;
     }
 
+	/**
+	 * Returns the note body in plain text.
+	 */
     public String getRawText() {
         return rawText;
     }
 
+	/**
+	 * Returns sketches associated with note.
+	 */
     public Bitmap getImage() {
         return mImage;
     }
 
+	/**
+	 * Returns the date and time last accessed as a java Date object.
+	 * @todo delete this
+	 */
     public Date getDateUpdated() {
         return dateUpdated;
     }
 
+	/**
+	 * Returns the date and time last accessed.
+	 * @todo change this to return last time edited.
+	 */
     public String getFormattedDateUpdatted() {
         return dt.format(dateUpdated);
     }
