1c6727a190426de6547009e2828af843ae5be002
gatchi <christen.got@gmail.com>
2017-07-22 16:51:50 -0700

    spaces -> tabs, added consistency

--- app/src/main/java/com/gatchi/notebooks/SaveOrUpdateNoteTask.java
+++ app/src/main/java/com/gatchi/notebooks/SaveOrUpdateNoteTask.java
@@ -13,21 +13,20 @@ import java.util.ArrayList;
  * Called by NoteActivity::saveOrUpdateNote.
  */
 public class SaveOrUpdateNoteTask extends AsyncTask<Note, Void, Void> {
+	private final Activity mCallingActivity;
+	private final DatabaseHandler mDbHandler;
+	private final boolean mIsUpdating;  // change this to "mUpdate" in next commit
 
-    private final Activity mCallingActivity;
-    private final DatabaseHandler mDbHandler;
-    private final boolean mIsUpdating;  // change this to "mUpdate" in next commit
-
-    /**
-     * @param callingActivity used to make Toasts/Dialogs on it
-     * @param databaseHandler database handler with calling activity context
-     * @param isUpdating boolean that is used to see if note is new or updated
-     */
-    public SaveOrUpdateNoteTask(Activity callingActivity, DatabaseHandler databaseHandler, boolean isUpdating) {
-        mCallingActivity = callingActivity;
-        mDbHandler = databaseHandler;
-        mIsUpdating = isUpdating;  // change "isUpdating" to "update" in next commit
-    }
+	/**
+	 * @param callingActivity used to make Toasts/Dialogs on it
+	 * @param databaseHandler database handler with calling activity context
+	 * @param isUpdating boolean that is used to see if note is new or updated
+	 */
+	public SaveOrUpdateNoteTask(Activity callingActivity, DatabaseHandler databaseHandler, boolean isUpdating) {
+		mCallingActivity = callingActivity;
+		mDbHandler = databaseHandler;
+		mIsUpdating = isUpdating;  // change "isUpdating" to "update" in next commit
+	}
 
 	/**
 	 * Does nothing.
@@ -35,25 +34,24 @@ public class SaveOrUpdateNoteTask extends AsyncTask<Note, Void, Void> {
 	 * This method is required by the superclass.
 	 * However, it need not be implemented.
 	 */
-    @Override
-    protected void onPreExecute() {
-    }
+	@Override
+	protected void onPreExecute() {
+	}
 
 	/**
 	 * Saves note (data side).
 	 * @see onPostExecute
 	 */
-    @Override
-    protected Void doInBackground(Note... params) {
-
-        if (mIsUpdating) {  // change to "mUpdate" in next commit
-            mDbHandler.updateNote(params[0]);
-        }
-        else {
-            mDbHandler.createNote(params[0]);
-        }
-        return null;
-    }
+	@Override
+	protected Void doInBackground(Note... params) {
+		if (mIsUpdating) {  // change to "mUpdate" in next commit
+			mDbHandler.updateNote(params[0]);
+		}
+		else {
+			mDbHandler.createNote(params[0]);
+		}
+		return null;
+	}
 
 	/**
 	 * Saves note (UI side).
@@ -65,16 +63,16 @@ public class SaveOrUpdateNoteTask extends AsyncTask<Note, Void, Void> {
 	 * (Android struggles to do GUI stuff outside main).
 	 * @see doInBackground
 	 */
-    @Override
-    protected void onPostExecute(Void result) {
-        ArrayList<Note> allNotes = mDbHandler.getAllNotesAsArrayList();
-        if (mIsUpdating) {  // change to "mUpdate" in next commit
-            Toast.makeText(mCallingActivity, mCallingActivity.getString(R.string.toast_note_updated), Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(mCallingActivity, mCallingActivity.getString(R.string.toast_note_created), Toast.LENGTH_SHORT).show();
-            MainActivity.noteAdapter.add(allNotes.get(mDbHandler.getNoteCount() - 1));
-        }
-        MainActivity.noteAdapter.setData(allNotes);
-        MainActivity.noteAdapter.notifyDataSetChanged();
-    }
+	@Override
+	protected void onPostExecute(Void result) {
+		ArrayList<Note> allNotes = mDbHandler.getAllNotesAsArrayList();
+		if (mIsUpdating) {  // change to "mUpdate" in next commit
+			Toast.makeText(mCallingActivity, mCallingActivity.getString(R.string.toast_note_updated), Toast.LENGTH_SHORT).show();
+		} else {
+			Toast.makeText(mCallingActivity, mCallingActivity.getString(R.string.toast_note_created), Toast.LENGTH_SHORT).show();
+			MainActivity.noteAdapter.add(allNotes.get(mDbHandler.getNoteCount() - 1));
+		}
+		MainActivity.noteAdapter.setData(allNotes);
+		MainActivity.noteAdapter.notifyDataSetChanged();
+	}
 }
